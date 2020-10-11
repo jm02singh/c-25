b7ab3f31446391e15957e03a16d7b734ef407dea
jm02singh <70170636+jm02singh@users.noreply.github.com>
2020-10-11 12:51:33 -0700

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,13 @@
+class Bird extends Baseclass{
+    constructor(x, y) {
+      super(x, y, 50, 50);
+      this.image = loadImage("sprites/Bird.png");
+    }
+    display(){
+      this.body.position.x = mouseX;
+      this.body.position.y = mouseY;
+      super.display()
+    }
+    }
+      
+  
